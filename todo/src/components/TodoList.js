c0c7ff211063a163007b4616daa8ccfc25cd27e0
mirage <mirage.kamran.pkt@gmail.com>
2020-04-23 22:30:34 -0700

    reducer created

---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import moment from "moment";
+
+export default function TodoList(props) {
+  console.log("list", props);
+
+  return (
+    <div>
+      {props.todo.map((item) => {
+        <li
+          className={`todo${item.done ? " done" : ""}`}
+          key={item.id}
+          onClick={() => props.toggle(item.id)}
+        >
+          {item.name} | added:{" "}
+          {moment(item.id).format("MMMM Do YYYY, h:mm:ss a")}
+        </li>;
+      })}
+      {props.todo[0] ? (
+        <button className="clear-btn" onClick={props.clear}>
+          Clear List
+        </button>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,37 @@
+export const initialState = {
+  todoItems: [
+    {
+      name: "Learn about reducers",
+      id: Date.now(),
+      completed: false,
+    },
+  ],
+};
+
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ...state,
+        todoItems: [
+          ...state.todoItems,
+          {
+            name: action.payload,
+            id: Date.now(),
+            completed: false,
+          },
+        ],
+      };
+    case "TOGGLE_ITEM":
+      return {
+        ...state,
+        todoItems: state.todoItems.map((item) => {
+          if (item.id === action.payload) {
+            return { ...item, dond: !item.done };
+          }else{
+              return item;
+          }
+        }),
+      };
+  }
+};
